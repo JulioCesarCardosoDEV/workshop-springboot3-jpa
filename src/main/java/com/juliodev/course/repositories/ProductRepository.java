8b7f39c2050a75ef6874cf41729c0714df67139e
Júlio César <julnhoo.cesarcontreras@gmail.com>
2024-05-07 14:12:10 -0300

    Many-to-many association with Product and Category

--- src/main/java/com/juliodev/course/repositories/ProductRepository.java
+++ src/main/java/com/juliodev/course/repositories/ProductRepository.java
@@ -2,7 +2,6 @@ package com.juliodev.course.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.juliodev.course.entities.Category;
 import com.juliodev.course.entities.Product;
 
 public interface ProductRepository extends JpaRepository<Product, Long>{
